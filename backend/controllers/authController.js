299789a8d167343e39729b39e9a0f571b78dcb45
karthikrajpoojari685-del <karthikrajpoojari685@gmail.com>
2026-07-07 23:18:48 +0530

    Added auth debug logs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -80,6 +80,9 @@ exports.login = catchAsyncErrors(async (req, res, next) => {
 
 
 // Protect Route
+console.log("req.headers.cookie:", req.headers.cookie);
+console.log("req.cookies:", req.cookies);
+console.log("origin:", req.headers.origin);
 exports.protect = catchAsyncErrors(async (req, res, next) => {
 
   let token;
